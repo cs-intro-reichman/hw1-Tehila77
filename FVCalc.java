ce834fe2f2d1e21894e40df283985e4063bd5ca6
Tehila77 <t.alaloof77@gmail.com>
2024-11-07 16:07:34 +0200

    done

--- FVCalc.java
+++ FVCalc.java
@@ -1,10 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		Double currentValue= Double.parseDouble(args [0]);
-		Double rate= Double.parseDouble(args [1]);
-		Double n= Double.parseDouble(args [2]);
-		Double futureValue= currentValue* (Math.pow(1+rate,n)); 
+		int currentValue= Integer.parseInt(args [0]);
+		double rate= Double.parseDouble(args [1]);
+		double rate1=rate/100;
+		int n= Integer.parseInt(args [2]);
+		double futureValue= currentValue* (Math.pow(1+ rate1,n)); 
 		System.out.println("After " + n +" years, " +currentValue+ "$ saved at "+rate +"% will yield " + futureValue);
-}
+	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,10 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	   int numberN =Integer.parseInt(args[0]);
+	   int a= numberN/100;
+	   int b=numberN/10%10;
+	   int c=numberN%10;
+       System.out.println(a + b +c );
 	}
 }
