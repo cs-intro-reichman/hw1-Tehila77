7a8d710450b53dcb39e4725f84b14fa4a1d9b1b7
Tehila77 <t.alaloof77@gmail.com>
2024-11-07 18:07:20 +0200

    done

--- NumWords.java
+++ NumWords.java
@@ -5,6 +5,6 @@ public class NumWords {
 	   int a= numberN/100;
 	   int b=numberN/10%10;
 	   int c=numberN%10;
-       System.out.println(a + b +c );
+       System.out.println(a +" "+ "hundreds ,"+ b +" "+" tens ,"+ c+" " +"ones.");
 	}
 }
