240b869690730587ca44d2b44218716f7d5faeea
Tehila77 <t.alaloof77@gmail.com>
2024-11-10 00:44:56 +0200

    fixed spaces

--- FVCalc.java
+++ FVCalc.java
@@ -2,14 +2,14 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue=Integer.parseInt(args [0]);
-		System.out.println(currentValue);
+		//System.out.println(currentValue);
 		double  rate=Double.parseDouble(args [1]);
-		System.out.println(rate);
+		//System.out.println(rate);
 		double rate1=rate/100;
-		System.out.println(rate1);
+		//System.out.println(rate1);
 		int n=Integer.parseInt(args [2]);
 	    double resultPow= (Math.pow(1+ rate1,n));
-		System.out.println(resultPow);
+		//System.out.println(resultPow);
 		double futureValue= (currentValue*resultPow);
 		System.out.println("After " + n +" years, " +currentValue+ "$ saved at "+rate +"% will yield " + futureValue);
 	}
