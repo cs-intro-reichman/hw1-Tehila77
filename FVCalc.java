f5bafab31dad7ad91f87c1c84879312f2301295a
Tehila77 <t.alaloof77@gmail.com>
2024-11-09 19:41:56 +0200

    fixed spaces and the word shekels

--- FVCalc.java
+++ FVCalc.java
@@ -1,11 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		int currentValue= Integer.parseInt(args [0]);
+		double currentValue= Double.parseDouble(args [0]);
 		double rate= Double.parseDouble(args [1]);
 		double rate1=rate/100;
-		int n= Integer.parseInt(args [2]);
-		double futureValue= currentValue* (Math.pow(1+ rate1,n)); 
+		double n=Double.parseDouble(args [2]);
+		//
+		double resultPow= (Math.pow(1+ rate1,n);
+		int futureValue= currentValue * resultPow;
 		System.out.println("After " + n +" years, " +currentValue+ "$ saved at "+rate +"% will yield " + futureValue);
 	}
 }
