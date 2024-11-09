7c2579c776876c1847ef8ac209449d9c5ddbab68
Tehila77 <t.alaloof77@gmail.com>
2024-11-09 22:28:12 +0200

    add printin

--- Ascend.java
+++ Ascend.java
@@ -3,11 +3,9 @@
 public class Ascend {
 	public static void main(String[] args) {
 	 int lim=Integer.parseInt(args[0]);
-	 System.out.println(lim);
 	 int a= (int)(Math.random()*lim);
 	 int b= (int)(Math.random()*lim);
 	 int c= (int)(Math.random()*lim);
-	System.out.println(a + " "+b + " "+c);
 	//find the min
 	 int cMin=Math.min(a,b);
 	 int theMin=Math.min(cMin,c);
@@ -17,5 +15,6 @@ public class Ascend {
 	 //find the medum
 	 int medum= Math.max(Math.min(a, b),c);
      System.out.println(theMax+" "+medum+" "+theMin);
+	 System.out.println(theMin+" "+medum+" "+theMax);
 	}
 }
