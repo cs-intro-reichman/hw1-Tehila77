486fa451e7fe2a99205f8cccaa59152a38d5c2e2
Tehila77 <t.alaloof77@gmail.com>
2024-11-10 00:28:01 +0200

    fixed spacs

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,5 +1,5 @@
 // Represents the hh:mm time format using an AM/PM format. 
-public class TimeFormat {
+public class TimeFormat {g
 	public static void main(String[] args) {
 		int hours = Integer.parseInt( ""+args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt(""+args[0].charAt(3) +args[0].charAt(4));
