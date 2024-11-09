5667ebb0e1e2e12a960e0a610405a51e2fa0e3f6
Tehila77 <t.alaloof77@gmail.com>
2024-11-09 20:12:56 +0200

    try to find the mistake - why the result uts 121.000001 and not 121

--- FVCalc.java
+++ FVCalc.java
@@ -2,12 +2,14 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue=Integer.parseInt(args [0]);
-		int  rate=Integer.parseInt(args [1]);
+		double  rate=Double.parseDouble(args [1]);
 		double rate1=rate/100;
+		System.out.println(rate1);
 		int n=Integer.parseInt(args [2]);
-		//
-		double resultPow= (Math.pow(1+ rate1,n));
-		double futureValue=currentValue*resultPow;
+	    double resultPow= (Math.pow(1+ rate1,n));
+		System.out.println(resultPow);
+		double futureValue= currentValue*resultPow;
+		System.out.println(futureValue);
 		System.out.println("After " + n +" years, " +currentValue+ "$ saved at "+rate +"% will yield " + futureValue);
 	}
 }
